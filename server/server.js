81f9ad92a86beff630ba0771fa15955b1e3ea157
Jeffrey Luu <83382029+jdluu@users.noreply.github.com>
2023-05-25 17:00:04 -0700

    update basic server

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 // Import required modules
 const express = require("express");
-
 // Create an instance of Express
 const app = express();
-
 // Define server port
 const port = 3000;
+// Use express.json() middleware to parse incoming request body as JSON
+app.use(express.json());
 
 // Define a route handler for the root URL
 app.get("/", (req, res) => {
