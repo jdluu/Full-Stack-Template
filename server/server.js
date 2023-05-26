fc6a8af80707194527f7c79a75a4d8f7ad03677e
Jeffrey Luu <83382029+jdluu@users.noreply.github.com>
2023-05-26 01:57:51 -0700

    integrate mongodb

--- server/server.js
+++ server/server.js
@@ -1,36 +1,59 @@
-// Import required modules
 const express = require("express");
 const mongoose = require("mongoose");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 
-// Create an instance of Express
 const app = express();
-
-// Define server port
 const port = 3001;
-
-// Use express.json() middleware to parse incoming request body as JSON
 app.use(express.json());
 
-// Define a route handler for the root URL
+// Connect to MongoDB using the MONGODB_URI environment variable
+const uri = process.env.MONGODB_URI;
+console.log(process.env);
+console.error("Connection string:", uri);
+mongoose
+	.connect(uri, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => {
+		console.log("Connected to MongoDB");
+		// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+		const client = new MongoClient(uri, {
+			serverApi: {
+				version: ServerApiVersion.v1,
+				strict: true,
+				deprecationErrors: true,
+			},
+		});
+
+		async function run() {
+			try {
+				// Connect the client to the server (optional starting in v4.7)
+				await client.connect();
+				// Send a ping to confirm a successful connection
+				await client.db("admin").command({ ping: 1 });
+				console.log(
+					"Pinged your deployment. You successfully connected to MongoDB!"
+				);
+			} catch (err) {
+				console.error("Error connecting to MongoDB:", err);
+			} finally {
+				// Ensures that the client will close when you finish/error
+				await client.close();
+			}
+		}
+
+		run().catch(console.dir);
+	})
+	.catch((err) => {
+		console.error("Error connecting to MongoDB:", err);
+	});
+
 app.get("/", (req, res) => {
 	res.send("Hello, World!");
 });
 
-// Connect to MongoDB using the MONGODB_URI environment variable
-mongoose.connect(
-	process.env.MONGODB_URI,
-	{ useNewUrlParser: true, useUnifiedTopology: true },
-	(err) => {
-		if (err) {
-			console.error(err);
-		} else {
-			console.log("Connected to MongoDB");
-		}
-	}
-);
-
-// Start the server and listen for incoming requests
 app.listen(port, () => {
 	console.log(`Server running on port ${port}`);
 });
