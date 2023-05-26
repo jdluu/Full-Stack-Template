451b901fe3cc5f4473b505d270d159782666f06c
Jeffrey Luu <83382029+jdluu@users.noreply.github.com>
2023-05-26 01:26:33 -0700

    integrate mongo

--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
 // Import required modules
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
 // Create an instance of Express
 const app = express();
+
 // Define server port
 const port = 3001;
+
 // Use express.json() middleware to parse incoming request body as JSON
 app.use(express.json());
 
@@ -11,6 +16,19 @@ app.use(express.json());
 app.get("/", (req, res) => {
 	res.send("Hello, World!");
 });
+
+// Connect to MongoDB using the MONGODB_URI environment variable
+mongoose.connect(
+	process.env.MONGODB_URI,
+	{ useNewUrlParser: true, useUnifiedTopology: true },
+	(err) => {
+		if (err) {
+			console.error(err);
+		} else {
+			console.log("Connected to MongoDB");
+		}
+	}
+);
 
 // Start the server and listen for incoming requests
 app.listen(port, () => {
